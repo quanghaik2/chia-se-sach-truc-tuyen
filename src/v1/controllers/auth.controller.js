0ece6bc4a0c26ab09028d68b2000b248673e1d5b
vootree <jsmsj04@gmail.com>
2023-10-23 13:15:37 +0700

    Fix auth cookie

--- src/v1/controllers/auth.controller.js
+++ src/v1/controllers/auth.controller.js
@@ -1,43 +1,71 @@
-const service =   require("../services");
+const service = require('../services');
 
-const register =async (req,res,next) =>{
-    try {
-        const user = await service.auth.register(req.body);
-        const {token,refreshToken,...data} = user;
-        res.cookie("token", {token},{ maxAge: 2*60*60000 });
-        res.cookie("refreshToken", {refreshToken},{ maxAge: 24*60*60000 });
-        res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
-}
+const register = async (req, res, next) => {
+   try {
+      const user = await service.auth.register(req.body);
+      const { token, refreshToken, ...data } = user;
+      res.cookie('token', { token }, { maxAge: 2 * 60 * 60000 });
+      res.cookie(
+         'refreshToken',
+         { refreshToken },
+         {
+            maxAge: 24 * 60 * 60000,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+         }
+      );
+      res.status(200).json(data);
+   } catch (error) {
+      next(error);
+   }
+};
 
-const login =async (req,res,next) =>{
-    try {
-        const user = await service.auth.login(req.body);
-        const {token,refreshToken,...data} = user;
-        res.cookie("token", {token},{ maxAge: 2*60*60000 });
-        res.cookie("refreshToken", {refreshToken},{ maxAge: 24*60*60000 });
-        res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
-}
+const login = async (req, res, next) => {
+   try {
+      const user = await service.auth.login(req.body);
+      const { token, refreshToken, ...data } = user;
+      res.cookie('token', { token }, { maxAge: 2 * 60 * 60000 });
+      res.cookie(
+         'refreshToken',
+         { refreshToken },
+         {
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60000,
+            httpOnly: true,
+            secure: true,
+         }
+      );
+      res.status(200).json(data);
+   } catch (error) {
+      next(error);
+   }
+};
 
 const refreshToken = async (req, res) => {
-    try {
-        const data = await service.auth.refreshToken(req.cookies.refreshToken.refreshToken);
-        const {token,refreshToken,...User} = data;
-        res.cookie("token", {token},{ maxAge: 2*60*60000 });
-        res.cookie("refreshToken", {refreshToken},{ maxAge: 24*60*60000 });
-        res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
-}
-module.exports ={
-    register,
-    login,
-    refreshToken,
-}
-
+   try {
+      const data = await service.auth.refreshToken(
+         req.cookies.refreshToken.refreshToken
+      );
+      const { token, refreshToken, ...User } = data;
+      res.cookie('token', { token }, { maxAge: 2 * 60 * 60000 });
+      res.cookie(
+         'refreshToken',
+         { refreshToken },
+         {
+            maxAge: 24 * 60 * 60000,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+         }
+      );
+      res.status(200).json(data);
+   } catch (error) {
+      next(error);
+   }
+};
+module.exports = {
+   register,
+   login,
+   refreshToken,
+};
