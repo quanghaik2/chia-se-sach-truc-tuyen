3e5f10b00617d95f000f6c4042dc93113724644e
quanghaik2 <chuquanghai2002@gmail.com>
2023-10-23 13:28:03 +0700

    update code

--- src/v1/controllers/auth.controller.js
+++ src/v1/controllers/auth.controller.js
@@ -1,30 +1,7 @@
 const service = require('../services');
 
-<<<<<<< HEAD
-const register =async (req,res,next) =>{
-    try {
-        const user = await service.auth.register(req.body);
-        const {token,refreshToken,...data} = user;
-        res.cookie("token", {token},{ maxAge: 2*60*60000 });
-        res.cookie("refreshToken", {refreshToken},{ maxAge: 24*60*60000 });
-        return res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
-}
 
-const login =async (req,res,next) =>{
-    try {
-        const user = await service.auth.login(req.body);
-        const {token,refreshToken,...data} = user;
-        res.cookie("token", {token},{ maxAge: 2*60*60000 });
-        res.cookie("refreshToken", {refreshToken},{ maxAge: 24*60*60000 });
-        return res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
-}
-=======
+
 const register = async (req, res, next) => {
    try {
       const user = await service.auth.register(req.body);
@@ -66,7 +43,7 @@ const login = async (req, res, next) => {
       next(error);
    }
 };
->>>>>>> 60f24bb1c98915bb4a16a51c50f9c5412d79f10b
+
 
 const refreshToken = async (req, res) => {
    try {
