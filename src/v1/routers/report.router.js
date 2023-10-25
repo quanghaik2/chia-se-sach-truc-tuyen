25d839460fc1d3a5f5dad368866d1090a4b10499
vootree <jsmsj04@gmail.com>
2023-10-25 11:57:34 +0700

    Handle report middlware

--- src/v1/routers/report.router.js
+++ src/v1/routers/report.router.js
@@ -1,13 +1,19 @@
 const { report } = require('../controllers');
+const middlewares = require('../middlewares');
 
 const router = require('express').Router();
 
-router.route('/list_by_userId/:userId').get(report.getListReportsByUserId);
+router
+   .route('/list_by_userId/:userId')
+   .get(middlewares.checkToken, report.getListReportsByUserId);
 router
    .route('/:reportId')
-   .patch(report.updateReport)
-   .get(report.getOneReportById)
-   .delete(report.deleteReport);
-router.route('/').get(report.getAllReports).post(report.createReport);
+   .patch(middlewares.checkToken, report.updateReport)
+   .get(middlewares.checkToken, report.getOneReportById)
+   .delete(middlewares.checkToken, report.deleteReport);
+router
+   .route('/')
+   .get(middlewares.checkToken, middlewares.checkRole, report.getAllReports)
+   .post(middlewares.checkToken, report.createReport);
 
 module.exports = router;
